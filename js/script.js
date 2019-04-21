fe08ede86a205a0a5e898a5c02994ad4b337e737
Shawn Conrad <shawnconrad@Shawns-MBP.attlocal.net>
2019-04-21 00:13:04 -0400

    arrays

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// ARRAYS: I can go through them with for loops
+var cars = ['Audi', 'Volvo', 'BMW'];
+cars.push('Toyota');
+cars.unshift('Ford');
+
+for (i = 0; i < cars.length; i++) {
+    document.write(cars[i] + '<br>');
+}
+
+
+// function addition (a, b) {
+//     var result = a + b;
+//     return result;
+// }
+
+// var output = addition('Shawn ', 'Conrad');
+// document.write(output);
+
+// for loop
+// for(var x =0; x < 10; x++) {
+//     document.write("This is written 10 times.<br>");
+// }
+
+// while loop
+// var x =1;
+
+// while(x < 10) {
+// document.write('<h1>' + x + '</h1>');
+// x+=2;
+// }
+
+// do while loop
+// var x = 7;
+
+// do{ 
+//     document.write("<h2>Freshco</h2>");
+//     x--;
+// } while(x > 4);
+
+// Switch statements
+
+/* var day = 6;
+
+switch(day) {
+    case 1:
+    document.write('<h2>Sunday</h2>');
+    break;
+    case 2:
+    document.write('<h2>Monday</h2>');
+    break;
+    case 3:
+    document.write('<h2>Tuesday</h2>');
+    break;
+    case 4:
+    document.write('<h2>Wednesday</h2>');
+    break;
+    case 5:
+    document.write('<h2>Thursday</h2>');
+    break;
+    case 6:
+    document.write('<h2>Friday</h2>');
+    break;
+    case 7:
+    document.write('<h2>Saturday</h2>');
+    break;
+    default: 
+    document.write('<h2>Wrong input.</h2>');
+
+} */
